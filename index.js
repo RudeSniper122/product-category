4db2ef0d033cc84e372fd398db18f6a83e70c79e
RudeSniper122 <85300776+RudeSniper122@users.noreply.github.com>
2021-11-02 12:21:21 +0530

    file upload

---
+++ index.js
@@ -0,0 +1,35 @@
+
+function myFunction1() {
+  document.getElementById("c1").classList.toggle("show");
+  document.getElementById("s1").classList.toggle("allSideCurveBorder");
+}
+
+function myFunction2() {
+  document.getElementById("c2").classList.toggle("show");
+  document.getElementById("s2").classList.toggle("allSideCurveBorder");
+}
+
+function myFunction3() {
+  document.getElementById("c3").classList.toggle("show");
+  document.getElementById("s3").classList.toggle("allSideCurveBorder");
+}
+
+function myFunction4() {
+  document.getElementById("c4").classList.toggle("show");
+  document.getElementById("s4").classList.toggle("allSideCurveBorder");
+}
+
+function myFunction5() {
+  document.getElementById("c5").classList.toggle("show");
+  document.getElementById("s5").classList.toggle("allSideCurveBorder");
+}
+
+function myFunction6() {
+  document.getElementById("c6").classList.toggle("show");
+  document.getElementById("s6").classList.toggle("allSideCurveBorder");
+}
+
+function myFunction7() {
+  document.getElementById("c7").classList.toggle("show");
+  document.getElementById("s7").classList.toggle("allSideCurveBorder");
+}
